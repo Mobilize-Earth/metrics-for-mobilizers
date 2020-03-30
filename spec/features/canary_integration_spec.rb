a390c8197fbccd94f9ae81e56c3f1b3869fcc9e6
richardlau <rilau@thoughtworks.com>
2020-03-30 15:39:48 -0400

    Create a landing page with root route

--- spec/features/canary_integration_spec.rb
+++ spec/features/canary_integration_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 feature "landing page is displayed" do
   scenario "when a user goes to the landing page" do
     visit '/'
-
-    it "shows content" do
-      expect(page).to have_content "Yay! You're on Rails!"
-    end
+    expect(page).to have_content "Climate Movement Reporting Tool"
   end
 end
