81f4271931fb226910144e9a12048c8cdff638b4
cortizTW <cortiz@thoughtworks.com>
2020-04-03 15:54:46 -0500

    Add dashboard contoller and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
 
     before_action :authenticate_user!
     skip_before_action :authenticate_user!, :only => [:index]
+
+    def after_sign_in_path_for(resource)
+        dashboard_index_url
+    end
 end
---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class DashboardController < ApplicationController
+    protect_from_forgery with: :exception
+
+    before_action :authenticate_user!
+end
