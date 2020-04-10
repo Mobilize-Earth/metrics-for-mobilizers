7b1b482a747247ec873b2f571707365484540941
cortizTW <cortiz@thoughtworks.com>
2020-04-09 19:09:47 -0500

    Improve login page acording to feedback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :registerable, :recoverable, :rememberable, :trackable and :omniauthable
-  devise :database_authenticatable, :validatable
+  # :confirmable, :lockable, :timeoutable, :registerable,  :rememberable, :trackable and :omniauthable
+  devise :database_authenticatable, :recoverable, :validatable
 
   validates :email, presence: true, :email => true
 end
