2ce2e0e6de5fc306ec06a0d54b7b422cb7e9a3ed
cortizTW <cortiz@thoughtworks.com>
2020-04-09 19:12:38 -0500

    mend

--- spec/features/login/login_integration_spec.rb
+++ spec/features/login/login_integration_spec.rb
@@ -4,8 +4,8 @@ feature 'login', :devise do
     scenario 'user redirect to login page' do
         visit_home_page
         click_link "Log In"
-        expect(page).to have_content "Log In"
-        # expect(page).to have_content "Forgot your password?"
+        expect(page).to have_content "Sign in"
+        expect(page).to have_content "Forgot your password?"
     end
 
     scenario 'user access login page' do
