4e030677be46a76a27e8b05a8a287045bcf0e300
Jeff Andolora <Jeffrey.Andolora@thoughtworks.com>
2020-04-14 15:39:45 -0400

    32 - Add admin index page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,4 +2,9 @@ class AdminsController < ApplicationController
     protect_from_forgery with: :exception
 
     before_action :authenticate_user!
+
+    def index
+      @chapters = Chapter.all
+      @users = User.all
+    end
 end
