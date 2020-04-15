35a482b14be207707cacbb0b1500a57156d69cad
richardlau <rilau@thoughtworks.com>
2020-04-14 21:33:49 -0400

    18 - Remove chapter index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
 #First user
 User.create!([
   { password: 'admin1', email: 'admin@test.com', role: 'admin' },
