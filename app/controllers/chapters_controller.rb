8a756f443680f536673019c12ba095bd2f463184
Rebecca Green <rebecca.green@thoughtworks.com>
2020-04-16 14:48:36 -0500

    [Reb/Jeff][#43] updates user validations, updates create and edit chapter form

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -7,12 +7,27 @@ class ChaptersController < ApplicationController
 
   def new
     @chapter = Chapter.new
-    @coordinators = User.where(role: "external")
+    @potential_coordinators = User.where(role: "external", chapter: nil)
   end
 
   def create
-    @chapter = Chapter.new(chapter_params)
+    params_chapter = params[:chapter]
+
+    @chapter = Chapter.new(
+      name: params_chapter[:name],
+      active_members: params_chapter[:active_members],
+      total_subscription_amount: params_chapter[:total_subscription_amount]
+    )
     if @chapter.save
+      coordinator_ids = params_chapter[:users]
+
+      if coordinator_ids
+        coordinator_ids.each do |id|
+          user = User.find(id)
+          user.update!(chapter: @chapter)
+        end
+      end
+
       redirect_to admins_index_path
     else
       flash[:errors] = @chapter.errors.full_messages
@@ -22,7 +37,8 @@ class ChaptersController < ApplicationController
 
   def edit
     @chapter = Chapter.find(params[:id])
-    @coordinators = User.where(role: "external")
+    @current_coordinators = User.where(role: "external", chapter: @chapter)
+    @potential_coordinators = User.where(role: "external", chapter: nil)
   end
 
   def update
@@ -40,5 +56,9 @@ class ChaptersController < ApplicationController
 
   def chapter_params
     params.require(:chapter).permit(:name, :active_members, :total_subscription_amount, :user_id)
+  end
+
+  def user_params
+    params.require(:chapter).permit(:users)
   end
 end
