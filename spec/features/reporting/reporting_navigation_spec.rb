0e7003d577ed4f7b78515b9c4aa3508f0fd7f320
Jose Zamudio <jose.zamudio@thoughtworks.com>
2020-04-16 09:53:37 -0500

    44- Disable tiles when user clicks no new data tile

--- spec/features/reporting/reporting_navigation_spec.rb
+++ spec/features/reporting/reporting_navigation_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 feature 'reporting done' do
+
   before(:each) do
     @user = FactoryBot.create(:user, role: 'external')
     sign_in(@user.email, @user.password)
     visit_home_page
     visit forms_index_path
   end
+
   scenario 'user redirected to dashboard when clicks done button' do
     click_link 'I\'m done'
     expect(page).to have_current_path dashboard_index_path, ignore_query: true
   end
+
+  xit 'skipping until we install selenium driver' do
+    scenario 'tiles should be disabled if use clicks on no new data tile' do
+
+      find('.no-data-tile').click
+      page.all('.form-tile').each do |tile|
+        tile.should have_css('.disabled')
+      end
+    end
+  end
+
 end
