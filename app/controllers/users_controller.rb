f41c85dd8f54d19dd8789d57f50f3b1a2494e33c
cortizTW <cortiz@thoughtworks.com>
2020-04-17 11:30:07 -0500

    7 - Solve a bug in the update message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @user.update(params.require(:user).permit(:first_name, :last_name, :email, :password, :role, :password_confirmation, :phone_number, :chapter_id))
         if @user.valid?
-          flash[:success] = "The user #{user.first_name} #{user.last_name} was successfully updated!"
+          flash[:success] = "The user #{@user.first_name} #{@user.last_name} was successfully updated!"
           redirect_to admins_index_path
         else
           flash[:errors] = @user.errors.full_messages
