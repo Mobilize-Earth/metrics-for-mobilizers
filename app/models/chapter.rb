3669c4d803bc2f82f4cbb33fda21a09a307e7b16
richardlau <rilau@thoughtworks.com>
2020-04-17 00:52:17 -0400

    43 - Fix unique chapter name error message

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,7 +2,7 @@ class Chapter < ApplicationRecord
   has_many :users
 
   validates :name, :active_members, :total_subscription_amount, presence: true
-  validates :name, uniqueness: { message: "This Chapter name has already been taken", case_sensitive: false }
+  validates :name, uniqueness: { message: "of this chapter has already been taken", case_sensitive: false }
   validates :active_members, numericality: { only_integer: true }
   validates :total_subscription_amount, numericality: true
 
