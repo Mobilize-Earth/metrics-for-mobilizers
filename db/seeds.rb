5b83c8c45813a00c67afc5b4bf9693fb013a9ce9
richardlau <rilau@thoughtworks.com>
2020-04-20 16:35:43 -0400

    db seed has external coordinators with chapters

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,16 @@ chapter = Chapter.create!(
   name: "Chapter 1", active_members: 10, total_subscription_amount: 100
 )
 
+#Admin and Consumer without chapters
 User.create!([
-  { password: 'external', password_confirmation: 'external', email: 'john@test.com', role: 'external', first_name: "John", last_name: "Smith", phone_number: "1" },
   { password: 'admin1', password_confirmation: 'admin1', email: 'admin@test.com', role: 'admin', first_name: "Admin", last_name: "Istrator", phone_number: "2"  },
-  { password: 'external', password_confirmation: 'external', email: 'external@test.com', role: 'external', first_name: "External", last_name: "User", phone_number: "3"  },
   { password: 'consumer', password_confirmation: 'consumer', email: 'consumer@test.com', role: 'consumer', first_name: "Consumer", last_name: "User", phone_number: "4"  }
+])
+
+#External coordinators with chapters
+chapter.users.create!([
+  { password: 'external', password_confirmation: 'external', email: 'external@test.com', role: 'external', first_name: "External", last_name: "User", phone_number: "3" },
+  { password: 'external', password_confirmation: 'external', email: 'john@test.com', role: 'external', first_name: "John", last_name: "Smith", phone_number: "1" }
 ])
 
 TrainingType.create!([
