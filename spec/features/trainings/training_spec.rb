0cd7f69d83fc1d0698dc3df380c035d321240d85
Jeff Andolora <Jeffrey.Andolora@thoughtworks.com>
2020-04-21 15:46:26 -0400

    27 - Reb/Jeff - Added feature tests for trainings

---
+++ spec/features/trainings/training_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+feature 'submitting training' do
+
+  before(:each) do
+    @user = FactoryBot.create(:user, role: 'external')
+    sign_in(@user.email, @user.password)
+    visit_home_page
+    visit trainings_path
+  end
+
+  scenario 'should show no form by default' do
+    expect(page).to have_content("Form will appear here")
+  end
+
+  scenario 'should show form after clicking on option' do
+    click_on 'Induction'
+
+    expect(page).not_to have_content("Form will appear here")
+  end
+
+  scenario 'should save to database for each valid training type option' do
+    count = 1
+    Training.training_type_options.each do |option|
+      click_on option
+      fill_in 'training_number_attendees', with: count
+      find('input[name="commit"]').click
+
+      expect(Training.last.number_attendees).to eq(count)
+      expect(Training.last.training_type).to eq(option)
+
+      count += 1
+    end
+  end
+
+  scenario 'with -1 participants should not save to database' do
+    number_of_records = Training.count
+
+    click_on 'Induction'
+    fill_in 'training_number_attendees', with: -1
+    find('input[name="commit"]').click
+
+    expect(Training.count).to eq(number_of_records)
+    expect(page).to have_css '.alert-danger'
+  end
+
+end
+
+feature 'navigation' do
+
+  before(:each) do
+    @user = FactoryBot.create(:user, role: 'external')
+    sign_in(@user.email, @user.password)
+    visit_home_page
+    visit trainings_path
+  end
+
+  scenario 'should navigate to forms index when user clicks finish' do
+    find('.test-finish-street-swarm').click
+    expect(page).to have_current_path forms_index_path, ignore_query: true
+  end
+
+  scenario 'should navigate to forms index when user clicks cancel' do
+    find('.test-cancel-street-swarm').click
+    expect(page).to have_current_path forms_index_path, ignore_query: true
+  end
+
+end
