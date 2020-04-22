3d988cb2341c0305d4dee65da26e12f267bc347f
Rebecca Green <rebecca.green@thoughtworks.com>
2020-04-22 15:34:06 -0500

    [48][Reb/Jeff] Adds ajax request to report tiles and tests for partial

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,15 @@ class ReportsController < ApplicationController
   end
 
   def tiles
+    report_data = {
+        members_total: 1,
+        members_growth: 7,
+        subscriptions: 1000,
+        arrestable_total: 1,
+        arrestable_attrition: 7,
+        arrests: 10
+    }
+    render json: report_data
   end
 
   def table
---
+++ spec/features/reports/reports_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature 'report tiles' do
+  before(:each) do
+    @user = FactoryBot.create(:user, role: 'external')
+    sign_in(@user.email, @user.password)
+    visit reports_path
+  end
+
+  scenario 'should contain corresponding data from the tiles api' do
+    expect(find('#members-total')).to have_content 1
+    expect(find('#members-growth')).to have_content 7
+    expect(find('#subscriptions')).to have_content 1000
+    expect(find('#arrestable-total')).to have_content 1
+    expect(find('#arrestable-attrition')).to have_content 7
+    expect(find('#arrests')).to have_content 10
+  end
+end
+
+feature 'reports page authorization' do
+  scenario 'should be accessible by external users' do
+    @user = FactoryBot.create(:user, role: 'external')
+    sign_in(@user.email, @user.password)
+    visit reports_path
+    expect(page).to have_css('.report-tiles-container')
+  end
+
+  scenario 'should be accessible by admin users' do
+    @user = FactoryBot.create(:user, role: 'admin', chapter: nil)
+    sign_in(@user.email, @user.password)
+    visit reports_path
+    expect(page).to have_css('.report-tiles-container')
+  end
+
+  # scenario 'should be accessible by reviewer users' do
+  #   @user = FactoryBot.create(:user, role: 'reviewer', chapter: nil)
+  #   sign_in(@user.email, @user.password)
+  #   visit reports_path
+  #   expect(page).to have_css('.report-tiles-container')
+  # end
+end
