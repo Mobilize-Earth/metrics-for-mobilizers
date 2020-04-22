ea26941e2ce95cecba911dcfa78cf4c010f61eac
Milber Champutiz Burbano <milber.ch@gmail.com>
2020-04-22 09:13:33 -0500

    [spike_location] renders json of states by country

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,5 +1,12 @@
 class LocationController < ApplicationController
   def show
     @countries = CS.get
+    @states = []
+  end
+
+  def state
+    country_code = params[:country_code]
+    @states = CS.states(country_code)
+    render json: {states: @states}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,5 @@ Rails.application.routes.draw do
 
   #location
   get '/location/show' => 'location#show'
+  get '/location/state' => 'location#state'
 end
