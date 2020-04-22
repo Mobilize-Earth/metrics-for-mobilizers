b39e5402d52a1cdcfbd4cb932deca358625264d0
Jose Zamudio <jose.zamudio@thoughtworks.com>
2020-04-22 18:34:20 -0500

    46-Show all countries

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,20 +4,22 @@ class ReportsController < ApplicationController
 
   def tiles
     report_data = {
-        members_total: 1,
-        members_growth: 7,
-        subscriptions: 1000,
-        arrestable_total: 1,
-        arrestable_attrition: 7,
-        arrests: 10
+      members_total: 1,
+      members_growth: 7,
+      subscriptions: 1000,
+      arrestable_total: 1,
+      arrestable_attrition: 7,
+      arrests: 10
     }
     render json: report_data
   end
 
   def table
-    countries = [{ country: "USA", members: 13 },
-                  { country: "Mexico", members: 12 },
-                  { country: "Ecuador", members: 543 }]
+    countries = CS.countries.map { |k, v| v }
+    countries = countries.map { |v| { country: v, members: 0 } }
+    countries.find { |v| v[:country] == 'Ecuador' }[:members] = 223
+    countries.find { |v| v[:country] == 'Mexico' }[:members] = 12
+    countries.find { |v| v[:country] == 'United States' }[:members] = 12323
     render json: countries
   end
 end
