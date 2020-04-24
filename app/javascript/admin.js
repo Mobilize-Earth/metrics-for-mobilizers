9d864c8c88d346028da8943613927e95bf2f34cb
Jeff Andolora <Jeffrey.Andolora@thoughtworks.com>
2020-04-24 14:55:57 -0400

    32 - Move admin JS to it's own file

---
+++ app/javascript/admin.js
@@ -0,0 +1,19 @@
+export default () => {
+    document.getElementById('users-nav-link').addEventListener('click', () => {
+        document.getElementById('content-users').style.display = 'block';
+        document.getElementById('link-users').style.display = 'block';
+        document.getElementById('users-nav-link').classList.add('selected');
+        document.getElementById('content-chapters').style.display = 'none';
+        document.getElementById('link-chapters').style.display = 'none';
+        document.getElementById('chapters-nav-link').classList.remove('selected');
+    });
+
+    document.getElementById('chapters-nav-link').addEventListener('click', () => {
+        document.getElementById('content-users').style.display = 'none';
+        document.getElementById('link-users').style.display = 'none';
+        document.getElementById('users-nav-link').classList.remove('selected');
+        document.getElementById('content-chapters').style.display = 'block';
+        document.getElementById('link-chapters').style.display = 'block';
+        document.getElementById('chapters-nav-link').classList.add('selected');
+    });
+}
--- app/javascript/features.js
+++ app/javascript/features.js
@@ -1,10 +1,14 @@
 import page from './page';
 import reports from './reports';
+import admin from './admin';
 
 $(document).on('turbolinks:load', () => {
     switch (page.controllerName()) {
         case 'reports':
             reports();
+            break;
+        case 'admins':
+            admin();
             break;
     }
 });
