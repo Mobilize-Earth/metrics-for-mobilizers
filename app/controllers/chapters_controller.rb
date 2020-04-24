c1c13f1c771f996850365ee1019f038e8c4e8f9f
richardlau <rilau@thoughtworks.com>
2020-04-23 22:49:29 -0400

    Squash some chapter address bugs

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -10,18 +10,26 @@ class ChaptersController < ApplicationController
     @chapter.build_address
     @current_coordinators = User.none
     authorize! :new, ChaptersController
-    @countries = CS.get.to_a
+    @countries = CS.get.to_a.sort { |a, b| a[1] <=> b[1]}
     @states = @cities = []
   end
 
   def create
     @chapter = Chapter.new(chapter_params)
+    @current_coordinators = User.none
+    @countries = CS.get.to_a.sort { |a, b| a[1] <=> b[1]}
+    chapter_country_code = CS.get.key(params[:chapter][:address_attributes][:country])
+    chapter_state_code = CS.states(chapter_country_code).key(params[:chapter][:address_attributes][:state_province])
+    @states = CS.states(chapter_country_code).to_a
+    @cities = CS.cities(chapter_state_code, chapter_country_code)
+    @states = [] if @states.blank?
+    @cities = [] if @cities.blank?
     if @chapter.save
       flash[:success] = "#{@chapter.name} was successfully created!"
       redirect_to admins_index_path
     else
       flash[:errors] = @chapter.errors.messages.values.flatten
-      redirect_to new_chapter_path
+      render :new
     end
   end
 
@@ -30,14 +38,13 @@ class ChaptersController < ApplicationController
     @chapter.build_address if @chapter.address.blank?
     @current_coordinators = User.where(role: "external", chapter: @chapter)
     authorize! :edit, ChaptersController
-    @countries = CS.get.to_a
+    @countries = CS.get.to_a.sort { |a, b| a[1] <=> b[1]}
     chapter_country_code = CS.get.key(@chapter.address.country)
     chapter_state_code = CS.states(chapter_country_code).key(@chapter.address.state_province)
     @states = CS.states(chapter_country_code).to_a
     @cities = CS.cities(chapter_state_code, chapter_country_code)
     @states = [] if @states.blank?
     @cities = [] if @cities.blank?
-
   end
 
   def update
