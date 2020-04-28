ebf8eb49d3f1e1fe42864f8abc6bf5dfcd342edb
Rebecca Green <rebecca.green@thoughtworks.com>
2020-04-28 13:29:54 -0500

    47 - Reb - Adds query for states and us regions

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -82,49 +82,49 @@ class ReportsController < ApplicationController
     response = if country.nil?
                  all_countries
                elsif country.upcase == 'US' && region.nil?
-                 mock_us_regions
+                 us_regions
                elsif country.upcase == 'US' && !region.nil? && state.nil?
-                 mock_us_states(region)
+                 us_states(region)
                elsif state.nil?
-                 mock_states(country)
+                 states(country)
                else
-                 mock_chapters(state)
+                 chapters(state)
                end
     render json: response
   end
 
   private
 
-  def mock_us_regions
+  def us_regions
     Regions.us_regions.map do |k, v|
       {
         id: k,
         region: v[:name],
-        members: 0,
-        chapters: 0,
-        signups: 0,
-        trainings: 0,
-        arrestable_pledges: 0,
-        actions: 0,
-        mobilizations: 0,
-        subscriptions: 0
+        members: Chapter.with_addresses.where(addresses: {state_province: v[:states] }).sum('active_members'),
+        chapters: Chapter.with_addresses.where(addresses: {state_province: v[:states]}).count,
+        signups: Mobilization.with_addresses.where(addresses: {state_province: v[:states]}).sum('new_members_sign_ons'),
+        trainings: Training.with_addresses.where(addresses: {state_province: v[:states]}).count,
+        arrestable_pledges: Mobilization.with_addresses.where(addresses: {state_province: v[:states]}).sum('arrestable_pledges'),
+        actions: calculate_actions(v[:states], :state_province),
+        mobilizations: Mobilization.with_addresses.where(addresses: {state_province: v[:states]}).count,
+        subscriptions: Mobilization.with_addresses.where(addresses: {state_province: v[:states]}).sum('xra_donation_suscriptions')
       }
     end
   end
 
-  def mock_us_states(region)
+  def us_states(region)
     Regions.us_regions[region.to_sym][:states].map do |v|
       {
         id: v,
         state: v,
-        members: 0,
-        chapters: 0,
-        signups: 0,
-        trainings: 0,
-        arrestable_pledges: 0,
-        actions: 0,
-        mobilizations: 0,
-        subscriptions: 0
+        members: Chapter.with_addresses.where(addresses: {state_province: v}).sum('active_members'),
+        chapters: Chapter.with_addresses.where(addresses: {state_province: v}).count,
+        signups: Mobilization.with_addresses.where(addresses: {state_province: v}).sum('new_members_sign_ons'),
+        trainings: Training.with_addresses.where(addresses: {state_province: v}).count,
+        arrestable_pledges: Mobilization.with_addresses.where(addresses: {state_province: v}).sum('arrestable_pledges'),
+        actions: calculate_actions(v, :state_province),
+        mobilizations: Mobilization.with_addresses.where(addresses: {state_province: v}).count,
+        subscriptions: Mobilization.with_addresses.where(addresses: {state_province: v}).sum('xra_donation_suscriptions')
       }
     end
   end
@@ -139,66 +139,60 @@ class ReportsController < ApplicationController
         signups: Mobilization.with_addresses.where(addresses: {country: v}).sum('new_members_sign_ons'),
         trainings: Training.with_addresses.where(addresses: {country: v}).count,
         arrestable_pledges: Mobilization.with_addresses.where(addresses: {country: v}).sum('arrestable_pledges'),
-        actions: calculate_actions(v),
+        actions: calculate_actions(v, :country),
         mobilizations: Mobilization.with_addresses.where(addresses: {country: v}).count,
         subscriptions: Mobilization.with_addresses.where(addresses: {country: v}).sum('xra_donation_suscriptions')
       }
     end
   end
 
-  def calculate_actions(country)
-    StreetSwarm.with_addresses.where(addresses: {country: country}).count +
-        ArrestableAction.with_addresses.where(addresses: {country: country}).count
+  def calculate_actions(name, type)
+    case type
+    when :country
+      StreetSwarm.with_addresses.where(addresses: {country: name}).count +
+          ArrestableAction.with_addresses.where(addresses: {country: name}).count
+    when :state_province
+      StreetSwarm.with_addresses.where(addresses: {state_province: name}).count +
+          ArrestableAction.with_addresses.where(addresses: {state_province: name}).count
+    when :chapter
+      StreetSwarm.where(chapter: name).count + ArrestableAction.where(chapter: name).count
+    end
   end
 
-  def mock_states(country)
+  def states(country)
     CS.states(country).map do |k, v|
       {
         id: k,
         state: v,
-        members: 0,
-        chapters: 0,
-        signups: 0,
-        trainings: 0,
-        arrestable_pledges: 0,
-        actions: 0,
-        mobilizations: 0,
-        subscriptions: 0
+        members: Chapter.with_addresses.where(addresses: {state_province: v}).sum('active_members'),
+        chapters: Chapter.with_addresses.where(addresses: {state_province: v}).count,
+        signups: Mobilization.with_addresses.where(addresses: {state_province: v}).sum('new_members_sign_ons'),
+        trainings: Training.with_addresses.where(addresses: {state_province: v}).count,
+        arrestable_pledges: Mobilization.with_addresses.where(addresses: {state_province: v}).sum('arrestable_pledges'),
+        actions: calculate_actions(v, :state_province),
+        mobilizations: Mobilization.with_addresses.where(addresses: {state_province: v}).count,
+        subscriptions: Mobilization.with_addresses.where(addresses: {state_province: v}).sum('xra_donation_suscriptions')
       }
     end
   end
 
-  def mock_chapters(state)
-    [
-      { id: 1, chapter: 'Chapter 1',
-        members: 20,
+  def chapters(state)
+    chapters_in_state = Chapter.with_addresses.where(addresses: {state_province: state})
+    chapters_in_state.map do |chapter|
+      { id: chapter.id,
+        chapter: chapter.name,
+        members: chapter.active_members,
         chapters: 1,
-        signups: 0,
-        trainings: 0,
-        arrestable_pledges: 0,
-        actions: 0,
-        mobilizations: 0,
-        subscriptions: 0 },
-      { id: 2, chapter: 'Chapter 2',
-        members: 420,
-        chapters: 1,
-        signups: 0,
-        trainings: 0,
-        arrestable_pledges: 0,
-        actions: 0,
-        mobilizations: 0,
-        subscriptions: 0 },
-      { id: 3, chapter: 'Chapter 3',
-        members: 43,
-        chapters: 1,
-        signups: 0,
-        trainings: 0,
-        arrestable_pledges: 0,
-        actions: 0,
-        mobilizations: 0,
-        subscriptions: 0 }
-    ]
+        signups: Mobilization.where(chapter: chapter).sum('new_members_sign_ons'),
+        trainings: Training.where(chapter: chapter).count,
+        arrestable_pledges: Mobilization.where(chapter: chapter).sum('arrestable_pledges'),
+        actions: calculate_actions(chapter, :chapter),
+        mobilizations: Mobilization.where(chapter: chapter).count,
+        subscriptions: Mobilization.where(chapter: chapter).sum('xra_donation_suscriptions')
+      }
+    end
   end
+
 
   def nowMinusDays(numDays)
     (DateTime.now - numDays.days).strftime("%d %B")
