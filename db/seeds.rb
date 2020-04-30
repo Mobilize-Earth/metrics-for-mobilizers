c84a0015eb108ac1c301035b8fe63410ce16da88
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-04-30 11:39:33 -0500

    47 - Dan/Rebecca - updated seeds.rb to include 5 US and 5 Global chapters with assosiated form submissions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-NUMBER_OF_CHAPTERS = 5
+NUMBER_OF_US_CHAPTERS = 5
+NUMBER_OF_GLOBAL_CHAPTERS = 5
+NUMBER_OF_FORMS_SUBMISSIONS = 5
 
+ArrestableAction.destroy_all
+Mobilization.destroy_all
+Training.destroy_all
+StreetSwarm.destroy_all
+Address.destroy_all
 User.destroy_all
 Chapter.destroy_all
+
 chapter = Chapter.create!(
   name: "Chapter 1", active_members: 10, total_subscription_amount: 100
 )
-chapter.create_address(country: "US", state_province: "NY", city: "Flushing", zip_code: "11040")
+chapter.create_address(country: "United States", state_province: "NY", city: "Flushing", zip_code: "11040")
 #Admin and Consumer without chapters
 User.create!([
   { password: 'admin1', password_confirmation: 'admin1', email: 'admin@test.com', role: 'admin', first_name: "Admin", last_name: "Istrator", phone_number: "2"  },
@@ -27,20 +35,77 @@ chapter.users.create!([
   { password: 'external', password_confirmation: 'external', email: 'john@test.com', role: 'external', first_name: "John", last_name: "Smith", phone_number: "1" }
 ])
 
-NUMBER_OF_CHAPTERS.times do |i|
-    state = Faker::Address.state
+def create_chapters(chapter_id, type)
+  state = Faker::Address.state
 
-    chapter = Chapter.create!(
-        name: "#{state} Chapter #{i}",
-        active_members: Faker::Number.number(digits: 3),
-        total_subscription_amount: Faker::Number.decimal(l_digits: 3, r_digits: 2)
+  chapter = Chapter.create!(
+      name: "#{state} Chapter #{chapter_id}",
+      active_members: Faker::Number.number(digits: 3),
+      total_subscription_amount: Faker::Number.decimal(l_digits: 3, r_digits: 2)
+  )
+
+  user = User.create!(
+      password: 'external',
+      password_confirmation: 'external',
+      email: Faker::Internet.email,
+      role: 'external',
+      first_name: Faker::Name.first_name ,
+      last_name: Faker::Name.last_name ,
+      phone_number: "1",
+      chapter: chapter
+  )
+
+  Address.create!(
+      country: type == 'US' ? 'United States' : Faker::Address.country,
+      state_province: state,
+      city: Faker::Address.city,
+      zip_code: Faker::Address.zip,
+      chapter: chapter
+  )
+
+  NUMBER_OF_FORMS_SUBMISSIONS.times do |i|
+    ArrestableAction.create!(
+        type_arrestable_action: 'Local (50+)',
+        xra_members: Faker::Number.number(digits: 2),
+        xra_not_members: Faker::Number.number(digits: 2),
+        trained_arrestable_present: Faker::Number.number(digits: 2),
+        arrested: Faker::Number.number(digits: 2),
+        chapter: chapter,
+        user: user
     )
 
-    Address.create!(
-        country: 'United States',
-        state_province: state,
-        city: Faker::Address.city,
-        zip_code: Faker::Address.zip,
-        chapter: chapter
+    Mobilization.create!(
+        participants: Faker::Number.number(digits: 2),
+        new_members_sign_ons: Faker::Number.number(digits: 2),
+        xra_donation_suscriptions: Faker::Number.number(digits: 2),
+        arrestable_pledges: Faker::Number.number(digits: 2),
+        mobilization_type: 'Rebel Ringing',
+        event_type: 'Virtual',
+        chapter: chapter,
+        user: user
     )
+
+    Training.create!(
+        number_attendees: Faker::Number.number(digits: 2),
+        chapter: chapter,
+        user: user,
+        training_type: 'NVDA'
+    )
+
+    StreetSwarm.create!(
+        xr_members_attended: Faker::Number.number(digits: 2),
+        chapter: chapter,
+        user: user
+    )
+  end
 end
+
+NUMBER_OF_US_CHAPTERS.times do |i|
+  self.create_chapters(i, 'US')
+end
+
+
+NUMBER_OF_GLOBAL_CHAPTERS.times do |i|
+  self.create_chapters(i, 'Global')
+end
+
