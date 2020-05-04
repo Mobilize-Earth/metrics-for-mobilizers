6fd6d7c9cc9322efc049fff13a98a2482315e9e8
richardlau <rilau@thoughtworks.com>
2020-05-04 09:28:44 -0400

    17 - Add report date to social media blitzing

--- app/controllers/social_media_blitzings_controller.rb
+++ app/controllers/social_media_blitzings_controller.rb
@@ -7,10 +7,14 @@ class SocialMediaBlitzingsController < ApplicationController
     end
 
     def create
+      date_params = params[:report_date]
+      localTime = DateTime.parse(date_params)
+      report_date = localTime.beginning_of_week
         @social_media_blitzing = SocialMediaBlitzing.new(
             user_id: current_user.id,
             chapter_id: current_user.chapter.id,
-            social_media_campaigns: params[:social_media_blitzing][:social_media_campaigns]
+            social_media_campaigns: params[:social_media_blitzing][:social_media_campaigns],
+            report_date: report_date
         )
         if @social_media_blitzing.save
             flash[:success] = "Social Media Blitzing data was successfully reported!"
