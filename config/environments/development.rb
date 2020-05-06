8ed6c153a5cf378575e7e92801dfaa98287e4fb5
cortizTW <cortiz@thoughtworks.com>
2020-05-06 14:40:20 -0500

    15 - Config sendmail as delivery method

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
