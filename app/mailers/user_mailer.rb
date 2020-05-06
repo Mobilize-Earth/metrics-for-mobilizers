fee29d19bf8c6e11fbbc6ff86ff628017f8dbce1
Jeff Andolora <Jeffrey.Andolora@thoughtworks.com>
2020-05-06 15:51:20 -0400

    3 - Custom email template and flavor text for forgot password form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ApplicationMailer
+  include Devise::Mailers::Helpers
+
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: 'Welcome to Reporting Tool')
+  end
+
+  def reset_password_instructions(recipient, argument, other_argument)
+    devise_mail(recipient, :reset_password_instructions)
   end
 end
---
+++ spec/features/login/forgot_password_integration_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'forgot password', :devise do
+  before :each do
+    @user = FactoryBot.create(:user)
+    visit_sign_in_page
+  end
+
+  it "sends email for forgot password" do
+    click_link "Forgot your password?"
+
+    fill_in 'user_email', with: @user.email
+
+    click_button "Email me a recovery link"
+
+    email_content = UserMailer.deliveries[0].body.encoded
+    expect(email_content).to include '<a href="http://localhost:3000/users/password/edit">Change my password</a>'
+    expect(email_content).to include '<img alt="MOBILIZE EARTH" src="data:image/png;base64,'  # Followed by long base64 content
+  end
+end
