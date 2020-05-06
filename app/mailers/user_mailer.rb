b8014aec7b6b70bc3411e23392655595e3ff613c
cortizTW <cortiz@thoughtworks.com>
2020-05-06 15:02:29 -0500

    15 - Refactors mail image

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   include Devise::Mailers::Helpers
+  before_action :add_inline_attachment!
 
   def welcome_email(user)
     @user = user
@@ -8,5 +9,9 @@ class UserMailer < ApplicationMailer
 
   def reset_password_instructions(recipient, argument, other_argument)
     devise_mail(recipient, :reset_password_instructions)
+  end
+  
+  def add_inline_attachment!
+    attachments.inline['footer_mail.png'] = File.read("#{Rails.root}/public/images/footer_mail.png")
   end
 end
