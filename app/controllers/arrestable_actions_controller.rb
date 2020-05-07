24632599b1810700ab1b57daf7d370d046b1ed95
Rebecca Green <rebecca.green@thoughtworks.com>
2020-05-07 09:23:07 -0500

    83 - Reb - moves street swarms and social media blitzing to action forms

--- app/controllers/arrestable_actions_controller.rb
+++ app/controllers/arrestable_actions_controller.rb
@@ -2,6 +2,8 @@ class ArrestableActionsController < ApplicationController
 
     def new
         @arrestable_action = ArrestableAction.new
+        @street_swarm = StreetSwarm.new
+        @social_media_blitzing = SocialMediaBlitzing.new
         @types = ArrestableAction.options
         authorize! :new, ArrestableActionsController
     end
