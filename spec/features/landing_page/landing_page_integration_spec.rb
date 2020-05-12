b42f437b78b47a1973652b33408a1d820ae37a96
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-05-12 16:49:42 -0500

    120 - Dan - added page footer and improved styles to home page

--- spec/features/landing_page/landing_page_integration_spec.rb
+++ spec/features/landing_page/landing_page_integration_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature "landing page is displayed" do
   scenario "when a user goes to the landing page" do
     visit_home_page
-    expect(page).to have_content "Lorem ipsum dolor sit amet"
+    expect(page).to have_content "Metrics for Mobilizers"
   end
 end
