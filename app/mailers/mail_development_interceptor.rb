23ff61aedf22e4142949cef29979d23c5f0d16b0
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-05-22 15:31:07 -0500

    removed unecessary instructions and config

--- app/mailers/mail_development_interceptor.rb
+++ app/mailers/mail_development_interceptor.rb
@@ -1,7 +1,7 @@
 class MailDevelopmentInterceptor
   def self.delivering_email(mail)
     if Rails.env.development? || Rails.env.staging?
-      mail.to = "staging.xr.reporting@gmail.com"
+      mail.to = "your-test-email@example.com"
       mail.cc = nil
       mail.bcc = nil
     end
